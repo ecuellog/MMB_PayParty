70c932e4be97a299a966475bd4662be0ce5714c0
Edgardo Cuello <ecuellog@gmail.com>
2017-09-22 00:30:21 -0700

    Landing page started

--- public/index.js
+++ public/index.js
@@ -1,8 +1,6 @@
 //import Vue from 'vue';
 //import App from '../views/index.vue'
 
-console.log('Inside');
-
 var app = new Vue({
   el: '#app',
   data: {
@@ -11,15 +9,51 @@ var app = new Vue({
 })
 
 new Vue({
-  el: '#navBar',
+  el: '#welcome',
   template: `
-  	<nav class='navbar fixed-top navbar-purple'>
-    	<a class='navbar-brand navbar-link fudo-home' href='#'> Fudo </a>
-		<a class='navbar-brand navbar-link' href='#'> Login </a>
-    </nav>
+    <div class='container-fluid'>
+      <div>
+      	<h1 class='global-font text-center transparent embossed-text'> Welcome! </h1> 
+        <h1 class='global-font text-center transparent embossed-text'> Bienvenido! </h1>
+      </div>
+      <div class='btn-div'>
+		    <button type='button' class='global-font button-pink btn-lg'> English </button>
+        <button type='button' class='global-font button-pink btn-lg'> Español </button>
+      </div>
+      <div id='countdown' class='global-font btn-div'>
+      </div>
+    </div>
   `
 })
 
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 11, 2017 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
 /* 
  * Notes: component.js
  * will include all components
