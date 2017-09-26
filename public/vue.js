6f29371172ad303d350711a2c54a9a4e8a9b54d1
Edgardo Cuello <ecuellog@gmail.com>
2017-09-25 23:19:08 -0700

    Paypal button added, spanish view content

--- public/vue.js
+++ public/vue.js
@@ -20,7 +20,7 @@ new Vue({
       </div>
       <div class='btn-div'>
 		    <button type='button' class='global-font button-pink btn-lg button-font'> English </button>
-        <button type='button' class='global-font button-pink btn-lg button-font'> Español </button>
+        <button type='button' onclick='window.location.href="/spanish"' class='global-font button-pink btn-lg button-font'> Español </button>
       </div>
       <div id='countdown' class='global-font btn-div'>
       </div>
@@ -28,14 +28,23 @@ new Vue({
   `
 });
 
-/*new Vue({
+new Vue({
   el: '#spanish',
   template: `
     <div class='container-fluid'>
-      <h1> Spanish View </h1>
+      <div id='countdown' class='global-font btn-div'></div>
+      <div class='party-info'>
+        <p class='global-font'>
+          Ha empezado la cuenta regresiva para la mejor fiesta del año en Punta Cana. Habran dos fiestas, una para las chicas y otra para los chicos.
+          La fiesta saldra en $116 dolares USD. Tambien esta la opcion de devolverse en el bus de fiestas al hotel, en lo cual costara $77 USD. Puedes ver
+          mas informacion sobre la fiesta <a href='#'>aqui</a>. Nuestro paquete es el blah blah.
+        </p>
+        <p class='global-font'> Se puede pagar por paypal haciendo click en "Pay Now" abajo o puedes enviar una transaccion Interac e-transfer al siguiente email
+          ecuellog@gmail.com.
+      </div>
     </div>
   `
-}):
+});
 
 new Vue({
   el: '#english',
@@ -44,7 +53,29 @@ new Vue({
       <h1> English View </h1>
     </div>
   `
-});*/
+});
+
+new Vue({
+  el: '#paypal-btn',
+  template:`
+    <div id='payButton'>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="hosted_button_id" value="YYESS7W4T8U32">
+        <table>
+        <tr><td><input type="hidden" name="on0" value="Options">Options</td></tr><tr><td><select name="os0">
+          <option value="Full Party">Full Party $116.00 USD</option>
+          <option value="Bus Only">Bus Only $77.00 USD</option>
+        </select> </td></tr>
+        </table>
+        <input type="hidden" name="currency_code" value="USD">
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+      </form>
+    </div>
+
+  `
+})
 
 /* Components */
 /*
@@ -96,5 +127,5 @@ var x = setInterval(function() {
 *   - hacer click en una opcion "radio button maybe"
 *   - boton de paypal
 *   - informacion de interac de papa
-/*
+*/
 
